1d9475219fc09396e863b785a75097ecb71daa5d
Guru K <gskglb@gmail.com>
2021-09-04 23:02:52 +0530

    Auth API Implemented

--- config.py
+++ config.py
@@ -12,17 +12,17 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
-MAIL_USERNAME = 'kachrakachra0@gmail.com'
-MAIL_PASSWORD = 'xxprvkiurepmbczo'
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
 MAIL_DEFAULT_SENDER = 'from@example.com'
 ACTIVATION_EXPIRE_DAYS = 2
 
 
 # Mongo DB Config
-DB_HOST = 'cluster0.jj6wh.mongodb.net'
+DB_HOST = ''
 DB_PORT = 27017
-DB_NAME = 'myFirstDatabase'
-DB_USER = 'testuser'
+DB_NAME = ''
+DB_USER = ''
 # move this to os.env
-DB_PASSWORD = 'test123'
+DB_PASSWORD = ''
 
